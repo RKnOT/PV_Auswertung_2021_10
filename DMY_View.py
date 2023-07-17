fb51d0645d2bd2897baf447a72ba2b6ad2d063d9
RKn <rudolf@knoll-clan.de>
2023-07-17 09:31:24 +0200

    csv added

--- DMY_View.py
+++ DMY_View.py
@@ -19,8 +19,8 @@ from CommonClasses.DataModel import GetCSV_File_Names as GetCSV_Names
 from helper_classes import check_CSV
 
 #-------Eingabe-----------
-DaySelected = 25
-MonthSelected = 6
+DaySelected = 14
+MonthSelected = 7
 YearSelected = 2023
 #------
 TeleSelected = 0
